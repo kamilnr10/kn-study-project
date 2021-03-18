bd61ae1f55818c598e5f5730c734406dc6254f30
kamilnr10 <kamil.nr10@gmail.com>
2021-03-18 20:00:13 +0100

    second commit

---
+++ src/data/Users.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    name: 'Kamil Bogdan',
+    attendance: '39%',
+    average: '2,3',
+  },
+  {
+    name: 'Tomek Adam',
+    attendance: '49%',
+    average: '2,9',
+  },
+  {
+    name: 'Adrian Eryk',
+    attendance: '23%',
+    average: '3,4',
+  },
+  {
+    name: 'Ela Kotlet',
+    attendance: '49%',
+    average: '2,5',
+  },
+];
